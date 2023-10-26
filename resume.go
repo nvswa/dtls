65f84f59f43257b2bf3493223e859b4fb7af96ef
Mingye Chen <mingyechen82@gmail.com>
2023-10-25 22:18:21 -0400

    Revert "Add ResumeWithContext"
    This reverts commit 12653fc9dbdc8f7583a9cda4101c2948ea851052.

--- resume.go
+++ resume.go
@@ -8,18 +8,12 @@ import (
 	"net"
 )
 
-// Resume imports an already established dtls connection using a specific dtls state.
-// If you want to specify the timeout duration, use ResumeWithContext() instead.
+// Resume imports an already established dtls connection using a specific dtls state
 func Resume(state *State, conn net.PacketConn, rAddr net.Addr, config *Config) (*Conn, error) {
-	return ResumeWithContext(context.Background(), state, conn, rAddr, config)
-}
-
-// ResumeWithContext imports an already established dtls connection using a specific dtls state.
-func ResumeWithContext(ctx context.Context, state *State, conn net.PacketConn, rAddr net.Addr, config *Config) (*Conn, error) {
 	if err := state.initCipherSuite(); err != nil {
 		return nil, err
 	}
-	c, err := createConn(ctx, conn, rAddr, config, state.isClient, state)
+	c, err := createConn(context.Background(), conn, rAddr, config, state.isClient, state)
 	if err != nil {
 		return nil, err
 	}
