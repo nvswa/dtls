4d850d910bd41d066f0337886c8b56dc077c7fff
Mingye Chen <mingyechen82@gmail.com>
2023-10-25 20:50:24 -0400

    Rm unneeded certs

--- examples/dial/resume/main.go
+++ examples/dial/resume/main.go
@@ -5,8 +5,6 @@
 package main
 
 import (
-	"crypto/tls"
-	"crypto/x509"
 	"fmt"
 	"log"
 	"net"
@@ -20,31 +18,16 @@ func main() {
 	// Prepare the IP to connect to
 	addr := &net.UDPAddr{IP: net.ParseIP("127.0.0.1"), Port: 4444}
 
-	//
-	// Everything below is the pion-DTLS API! Thanks for using it ❤️.
-	//
-
-	certificate, err := util.LoadKeyAndCertificate("examples/certificates/client.pem",
-		"examples/certificates/client.pub.pem")
-	util.Check(err)
-
-	rootCertificate, err := util.LoadCertificate("examples/certificates/server.pub.pem")
-	util.Check(err)
-	certPool := x509.NewCertPool()
-	cert, err := x509.ParseCertificate(rootCertificate.Certificate[0])
-	util.Check(err)
-	certPool.AddCert(cert)
-
 	// Prepare the configuration of the DTLS connection
 	config := &dtls.Config{
-		Certificates:         []tls.Certificate{certificate},
 		ExtendedMasterSecret: dtls.RequireExtendedMasterSecret,
-		RootCAs:              certPool,
 		KeyLogWriter:         log.Default().Writer(),
 	}
 
 	// Connect to a DTLS server
 	readData, err := os.ReadFile("resume.bin")
+	util.Check(err)
+
 	state := &dtls.State{}
 	err = state.UnmarshalBinary(readData)
 	util.Check(err)
