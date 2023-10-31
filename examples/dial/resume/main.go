8668f231429b5d1e54c30f3c72496c7b72ab8568
Mingye Chen <mingyechen82@gmail.com>
2023-10-30 23:15:30 -0400

    Prepend stuff

--- examples/dial/resume/main.go
+++ examples/dial/resume/main.go
@@ -8,14 +8,20 @@ import (
 	"log"
 	"net"
 	"os"
+	"sync"
 
 	"github.com/pion/dtls/v2"
 	"github.com/pion/dtls/v2/examples/util"
+	"github.com/pion/dtls/v2/pkg/protocol"
+	"github.com/pion/dtls/v2/pkg/protocol/recordlayer"
 )
+
+const cidSize = 8
 
 func main() {
 	var remoteAddr = flag.String("raddr", "127.0.0.1:4444", "remote address")
 	var resumeFile = flag.String("file", "", "resume file")
+	var prepend = flag.String("prepend", "", "to prepend")
 	var secret = flag.String("secret", "1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef", "shared secret")
 	flag.Parse()
 
@@ -46,7 +52,12 @@ func main() {
 	pConn, err := net.ListenUDP("udp", nil)
 	util.Check(err)
 
-	dtlsConn, err := dtls.Resume(state, pConn, raddr, config)
+	wpconn := &write1pconn{
+		PacketConn: pConn,
+		onceBytes:  []byte(*prepend),
+	}
+
+	dtlsConn, err := dtls.Resume(state, wpconn, raddr, config)
 
 	util.Check(err)
 	defer func() {
@@ -56,4 +67,77 @@ func main() {
 	fmt.Println("Connected; type 'exit' to shutdown gracefully")
 
 	util.Chat(dtlsConn)
+}
+
+type write1pconn struct {
+	net.PacketConn
+	onceBytes []byte
+	doOnce    sync.Once
+}
+
+func (c *write1pconn) WriteTo(p []byte, addr net.Addr) (int, error) {
+	var n int
+	var err error
+	c.doOnce.Do(func() {
+		var new []byte
+		new, err = c.editBuf(p)
+		if err != nil {
+			return
+		}
+
+		n, err = c.PacketConn.WriteTo(new, addr)
+	})
+	if err != nil {
+		return 0, err
+	}
+	if n > 0 {
+		return n, nil
+	}
+
+	return c.PacketConn.WriteTo(p, addr)
+}
+
+func (c *write1pconn) editBuf(p []byte) ([]byte, error) {
+	pkts, err := recordlayer.ContentAwareUnpackDatagram(p, cidSize)
+	if err != nil {
+		return nil, err
+	}
+
+	h := &recordlayer.Header{
+		ConnectionID: make([]byte, cidSize),
+	}
+	for i, pkt := range pkts {
+		if err := h.Unmarshal(pkt); err != nil {
+			continue
+		}
+
+		if h.ContentType != protocol.ContentTypeConnectionID {
+			continue
+		}
+
+		start := recordlayer.FixedHeaderSize + cidSize
+		appData := pkt[start:]
+
+		h.ContentLen = uint16(len(c.onceBytes) + len(appData))
+
+		newHeader, err := h.Marshal()
+		if err != nil {
+			return nil, err
+		}
+
+		combined := make([]byte, 0, len(newHeader)+len(c.onceBytes)+len(appData))
+		combined = append(combined, newHeader...)
+		combined = append(combined, c.onceBytes...)
+		combined = append(combined, appData...)
+
+		pkts[i] = combined
+	}
+
+	var flatData []byte
+	for _, d := range pkts {
+		flatData = append(flatData, d...)
+	}
+
+	return flatData, nil
+
 }
